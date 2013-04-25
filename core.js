9c9a0aa9993219a1c35620ce2a7f1c8695c6191f
Derek Schindhelm <ddschindhelm@gmail.com>
2013-04-25 11:27:06 +0200

    fb

--- core.js
+++ core.js
@@ -102,7 +102,7 @@ var firstVid = true;
 function video(){
 	if(firstVid){
 		firstVid = false;
-		$('#myModal').html("<div class='flex-video'><iframe src='https://www.facebook.com/video/embed?video_id=10200384229921907' width='1280' height='720' style='border-width:0px' ></iframe></div>");
+		$('.flex-video').html("<iframe src='https://www.facebook.com/video/embed?video_id=10200384229921907' width='1280' height='720' style='border-width:0px' ></iframe>");
 	}
 	
 }
